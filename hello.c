9a03d24f6c112710a44a3a01fc99277e78a446b0
AkumuJey <josephowino7@gmail.com>
2023-02-13 14:56:48 +0300

    Started cs50 classes

---
+++ hello.c
@@ -0,0 +1,9 @@
+
+#include<stdio.h>
+
+
+int main(void)
+{
+    int mine = 25;
+    printf("Hello, Jey\n %i", mine);
+}
---
+++ score.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+    int score_1 = 72;
+    int score_2 = 73;
+    int score_3 = 33;
+    printf("Average: %f\n", (score_1 + score_2 + score_3)/3.0);
+}
