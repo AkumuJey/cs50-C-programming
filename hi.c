b8703a4cbdf756d9ae73cad956c116c439c04585
AkumuJey <josephowino7@gmail.com>
2023-02-13 17:11:45 +0300

    Structure of string. Zero added at end of string

---
+++ hi.c
@@ -0,0 +1,10 @@
+int main(void) {
+     char str[2];
+     char nm[2];
+    printf("Enter something: ");
+    scanf("%s", &str);
+    printf("Enter something: ");
+    scanf("%s", &nm);
+    printf("%i %i %i %i", str[0], str[1], str[2]);
+    printf("%i %i %i %i", nm[0], nm[1], nm[2]);
+};
--- score.c
+++ score.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void) {
-    int score_1 = 72;
-    int score_2 = 73;
-    int score_3 = 33;
-    printf("Average: %f\n", (score_1 + score_2 + score_3)/3.0);
+    int n;
+    printf("How many numbers do you want to average? ");
+    scanf("%d", &n);
+    
+    int scores[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter the scores: ");
+        scanf("%d", &scores[i]);
+    }
+    
+    printf("Average: %f\n", (scores[0] + scores[1]+ scores[2])/3.0);
 }
